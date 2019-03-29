60a7b6807540ec07f20efa79b32d51cfedb88d16
Andres <ber171504@uvg.edu.gt>
2019-03-28 19:40:07 -0700

    idk

--- src/src/main.java
+++ src/src/main.java
@@ -9,20 +9,23 @@ import java.util.Scanner ;
 import java.io.*;
 public class main {
     public static void main(String [] args) throws IOException {
-        VectorHeap vectorHeap = new VectorHeap();
-        Scanner input = new Scanner(System.in);
+        VectorHeap vectorHeap = new VectorHeap();// crea el vector heap
+        Scanner input = new Scanner(System.in);// crea el scanner
         System.out.println("ingrese donde se localiza el archivo con los pacientes: ");// bufferReader codigo basado de los ejemplos encontrados en la pagina : https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
-        String fileName = input.next();
-        File file = new File(fileName);
+        String fileName = input.next();// se pide el ingreso de la direccion del file con el texto ejemplo: C:\Users\ticoa\OneDrive\Desktop\Cosas\EstructuraDeDatos\Tarea8\Pacientes.txt
+        File file = new File(fileName);// crea el file
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
         String line;
-        while ((line = br.readLine()) != null) {
+        while ((line = br.readLine()) != null) { //crea while donde se agregan los pacientes al vector heap
             //process the line
             String[] parts = line.split(",");// se separa el string que da el buffer reader
-            Paciente pac = new Paciente(parts[0],parts[1],parts[2]);
-            vectorHeap.add((Comparable) pac);
+            Paciente pac = new Paciente(parts[0],parts[1],parts[2]); // crea al paciente con sus 3 atributos
+            vectorHeap.add((Comparable) pac); // agrega al paciente al vector heap
         }
-        
+        System.out.println("Lo siguiente son los pacientes ordenados");
+        while (!vectorHeap.isEmpty()){ // while que continua hasta que el vector heap este vacio
+            vectorHeap.remove(); // imprime y quita elementos del vector heap en orden
+        }
     }
 }
